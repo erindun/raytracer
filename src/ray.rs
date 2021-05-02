4416c22f67fdfa09dac5ca35261c1d0a8aa285d6
Garrett Duncan <garrettdunc@gmail.com>
2021-05-02 18:23:57 -0400

    Add function to generate ray color

--- src/ray.rs
+++ src/ray.rs
@@ -1,3 +1,4 @@
+use crate::color::Color;
 use crate::vec3::Vec3;
 
 struct Ray {
@@ -13,4 +14,15 @@ impl Ray {
     fn at(&self, t: f32) -> Vec3 {
         self.origin + t * self.direction
     }
+}
+
+fn gen_ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction.normal();
+    let t = 0.5 * Vec3::new(unit_direction.x, unit_direction.y + 1.0, unit_direction.z);
+
+    let r = (1.0 * (1.0 - t.x)) + t.x * 0.5;
+    let g = (1.0 * (1.0 - t.y)) + t.y * 0.7;
+    let b = (1.0 * (1.0 - t.z)) + t.z * 1.0;
+
+    Color::new(r, g, b)
 }
