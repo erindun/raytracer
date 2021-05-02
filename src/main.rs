8b0b26cba35c54240fb7e8fbcbfcb9b76b4b3e47
Garrett Duncan <garrettdunc@gmail.com>
2021-05-02 19:03:31 -0400

    Prepare to cast rays

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,41 @@ mod ray;
 mod vec3;
 
 use color::Color;
+use vec3::Vec3;
 use std::fs::File;
 use std::io::{self, Write};
 
 fn main() {
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    // Set the image size.
+    let aspect: f32 = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f32 / aspect) as i32;
+
+    // Set the camera.
+    let viewport_height: f32 = 2.0;
+    let viewport_width: f32 = aspect * viewport_height;
+    let focal_length: f32 = 1.0;
+
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     let mut f = File::create("output.ppm").expect("Error creating file.");
 
     // PPM file header.
-    writeln!(f, "P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT).expect("Error writing to file.");
+    writeln!(f, "P3\n{} {}\n255", image_width, image_height).expect("Error writing to file.");
 
-    for j in (0..IMAGE_HEIGHT).rev() {
-        for i in 0..IMAGE_WIDTH {
+    // Render the image.
+    for j in (0..image_height).rev() {
+        for i in 0..image_width {
             print!("\rScanlines remaining: {}", j);
             print!(" ");
             io::stdout().flush().unwrap();
 
             let color = Color::new(
-                i as f32 / (IMAGE_WIDTH - 1) as f32,
-                j as f32 / (IMAGE_HEIGHT - 1) as f32,
+                i as f32 / (image_width - 1) as f32,
+                j as f32 / (image_height - 1) as f32,
                 0.25,
             );
 
