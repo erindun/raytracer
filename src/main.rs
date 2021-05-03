0c9dd667cca9e7dba945e3dde05c13f7e3c34fa0
Garrett Duncan <garrettdunc@gmail.com>
2021-05-03 01:10:24 -0400

    Save work

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,27 @@ mod ray;
 mod vec3;
 
 use color::Color;
-use vec3::Vec3;
+use ray::Ray;
 use std::fs::File;
 use std::io::{self, Write};
+use vec3::Vec3;
 
 fn main() {
     // Set the image size.
-    let aspect: f32 = 16.0 / 9.0;
+    let aspect = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f32 / aspect) as i32;
 
     // Set the camera.
-    let viewport_height: f32 = 2.0;
-    let viewport_width: f32 = aspect * viewport_height;
+    let viewport_height = 2.0;
+    let viewport_width = aspect * viewport_height;
     let focal_length: f32 = 1.0;
 
     let origin = Vec3::new(0.0, 0.0, 0.0);
     let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
     let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     let mut f = File::create("output.ppm").expect("Error creating file.");
 
@@ -30,18 +32,19 @@ fn main() {
 
     // Render the image.
     for j in (0..image_height).rev() {
+        print!("\rScanlines remaining: {}", j);
+        print!(" ");
+        io::stdout().flush().unwrap();
         for i in 0..image_width {
-            print!("\rScanlines remaining: {}", j);
-            print!(" ");
-            io::stdout().flush().unwrap();
-
-            let color = Color::new(
-                i as f32 / (image_width - 1) as f32,
-                j as f32 / (image_height - 1) as f32,
-                0.25,
+            let u = i as f32 / ((image_width - 1) as f32);
+            let v = j as f32 / ((image_height - 1) as f32);
+            let ray = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let pixel_color = ray::gen_ray_color(&ray);
 
-            writeln!(f, "{}", color).expect("Error writing to file.");
+            writeln!(f, "{}", pixel_color).expect("Error writing to file.");
         }
     }
 }
--- src/ray.rs
+++ src/ray.rs
@@ -1,28 +1,27 @@
 use crate::color::Color;
 use crate::vec3::Vec3;
 
-struct Ray {
+pub struct Ray {
     origin: Vec3,
     direction: Vec3,
 }
 
 impl Ray {
-    fn new(origin: Vec3, direction: Vec3) -> Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray {
         Ray { origin, direction }
     }
 
-    fn at(&self, t: f32) -> Vec3 {
+    pub fn at(&self, t: f32) -> Vec3 {
         self.origin + t * self.direction
     }
 }
 
-fn gen_ray_color(ray: &Ray) -> Color {
+pub fn gen_ray_color(ray: &Ray) -> Color {
     let unit_direction = ray.direction.normal();
-    let t = 0.5 * Vec3::new(unit_direction.x, unit_direction.y + 1.0, unit_direction.z);
+    let t = 0.5 * (unit_direction.y + 1.0);
 
-    let r = (1.0 * (1.0 - t.x)) + t.x * 0.5;
-    let g = (1.0 * (1.0 - t.y)) + t.y * 0.7;
-    let b = (1.0 * (1.0 - t.z)) + t.z * 1.0;
-
+    let r = (1.0 * (1.0 - t)) + t * 0.5;
+    let g = (1.0 * (1.0 - t)) + t * 0.7;
+    let b = (1.0 * (1.0 - t)) + t * 1.0;
     Color::new(r, g, b)
 }
